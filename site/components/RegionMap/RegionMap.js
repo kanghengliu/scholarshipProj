08ecc58ca4394607488125437c10065585200797
kanghengliu <kl1099@georgetown.edu>
2024-05-17 22:34:01 -0400

    feat: Markers show for only one div

--- site/components/RegionMap/RegionMap.js
+++ site/components/RegionMap/RegionMap.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, GeoJSON, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, GeoJSON, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import * as turf from '@turf/turf';
 import 'leaflet/dist/leaflet.css';
-import { createPortal } from 'react-dom';
 
 // Custom marker component
 const CustomMarker = ({ position, idx, show, delay }) => {
     const map = useMap();
     const markerRef = React.useRef(null);
+    const [markerVisible, setMarkerVisible] = useState(false);
 
     useEffect(() => {
         if (show && !markerRef.current) {
@@ -21,8 +21,21 @@ const CustomMarker = ({ position, idx, show, delay }) => {
                     html: markerDiv.outerHTML,
                 }),
             }).addTo(map);
+            setMarkerVisible(true);
+        } else if (!show && markerRef.current && markerVisible) {
+            const markerDiv = markerRef.current.getElement();
+            if (markerDiv) {
+                markerDiv.className = 'custom-marker marker-fade-out';
+                setTimeout(() => {
+                    if (markerRef.current) {
+                        map.removeLayer(markerRef.current);
+                        markerRef.current = null;
+                        setMarkerVisible(false);
+                    }
+                }, 150); // Match the duration of the fade-out animation
+            }
         }
-    }, [show, map, position, delay]);
+    }, [show, map, position, delay, markerVisible]);
 
     return null;
 };
@@ -120,67 +133,74 @@ const SimpleMap = ({ mapTriggerId, markerTriggerId }) => {
 
     return (
         <>
-        <style jsx global>{`
-            .leaflet-container {
-                background-color: transparent !important;
-                pointer-events: none;  // Make sure pointer events are on for interactivity
-            }
-            .leaflet-interactive {
-                pointer-events: none;  // Disable interactivity over polygons and markers
-                // visibility: hidden; // Hide polygons and markers
-                cursor: none; // Restore interactivity over polygons and markers
-            }
-            .custom-marker {
-                width: 25px;
-                height: 41px;
-                background: url('https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png') no-repeat center;
-                background-size: contain;
-                opacity: 0; // Start with opacity 0
-                transform: translateY(-20px); // Start with position above
-            }
-            .marker-fade-in {
-                animation: fadeIn 0.15s ease-out forwards;
-            }
-            @keyframes fadeIn {
-                0% { opacity: 0; transform: translateY(-20px); }
-                100% { opacity: 1; transform: translateY(0); }
-            }
-        `}</style>
-        <div id="map-container" style={{ visibility: hasMapBeenTriggered ? 'visible' : 'hidden' }}>
-            <MapContainer
-                center={mapCenter}
-                zoom={13}
-                scrollWheelZoom={false}
-                style={{ height: "600px", width: "800px" }}
-                zoomControl={false}
-                dragging={false}
-                doubleClickZoom={false}
-                touchZoom={false}
-                keyboard={false}
-                boxZoom={false}
-                attributionControl={false}
-            >
-                {geoJsonData && <GeoJSON
-                    data={geoJsonData}
-                    style={() => ({
-                        color: 'red',
-                        fillColor: '#f03',
-                        fillOpacity: 0.5,
-                        weight: 5,
-                        interactive: false
-                    })}
-                />}
-                {pointData && pointData.features.map((feature, idx) => (
-                    <CustomMarker
-                        key={idx}
-                        position={[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]}
-                        show={showMarkers}
-                        idx={idx}
-                        delay={idx * 0.015} // Delay based on index
-                    />
-                ))}
-            </MapContainer>
-        </div>
+            <style jsx global>{`
+                .leaflet-container {
+                    background-color: transparent !important;
+                    pointer-events: none;  // Make sure pointer events are on for interactivity
+                }
+                .leaflet-interactive {
+                    pointer-events: none;  // Disable interactivity over polygons and markers
+                    // visibility: hidden; // Hide polygons and markers
+                    cursor: none; // Restore interactivity over polygons and markers
+                }
+                .custom-marker {
+                    width: 25px;
+                    height: 41px;
+                    background: url('https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png') no-repeat center;
+                    background-size: contain;
+                    opacity: 0; // Start with opacity 0
+                    transform: translateY(-20px); // Start with position above
+                }
+                .marker-fade-in {
+                    animation: fadeIn 0.15s ease-out forwards;
+                }
+                .marker-fade-out {
+                    animation: fadeOut 0s ease-out forwards;
+                }
+                @keyframes fadeIn {
+                    0% { opacity: 0; transform: translateY(-20px); }
+                    100% { opacity: 1; transform: translateY(0); }
+                }
+                @keyframes fadeOut {
+                    0% { opacity: 1; transform: translateY(0); }
+                    100% { opacity: 0; transform: translateY(-20px); }
+                }
+            `}</style>
+            <div id="map-container" style={{ visibility: hasMapBeenTriggered ? 'visible' : 'hidden' }}>
+                <MapContainer
+                    center={mapCenter}
+                    zoom={13}
+                    scrollWheelZoom={false}
+                    style={{ height: "600px", width: "800px" }}
+                    zoomControl={false}
+                    dragging={false}
+                    doubleClickZoom={false}
+                    touchZoom={false}
+                    keyboard={false}
+                    boxZoom={false}
+                    attributionControl={false}
+                >
+                    {geoJsonData && <GeoJSON
+                        data={geoJsonData}
+                        style={() => ({
+                            color: 'red',
+                            fillColor: '#f03',
+                            fillOpacity: 0.5,
+                            weight: 5,
+                            interactive: false
+                        })}
+                    />}
+                    {pointData && pointData.features.map((feature, idx) => (
+                        <CustomMarker
+                            key={idx}
+                            position={[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]}
+                            show={showMarkers}
+                            idx={idx}
+                            delay={idx * 0.015} // Delay based on index
+                        />
+                    ))}
+                </MapContainer>
+            </div>
         </>
     );
 };
